b7f4b5ddf8624f0380385628052611604d4252c8
aemoji <74324934+aemoji@users.noreply.github.com>
2022-06-21 09:41:01 +0530

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,8 +1,5 @@
-public class JenkinsDemo{
-
-public static void main(String[] args){
-  
+class JenkinsDemo{
+public static void main(String args[]){
   System.out.println("My first jenkins");
-
-}
+  }
 }
